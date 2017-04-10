dc7177028f4647afaf66167b0aca2519748d97d6
Roland <MarksVision@163.com>
2017-04-10 09:17:18 +0800

    init commit

---
+++ app/src/main/java/net/oschina/git/roland/wimm/WellComeActivity.java
@@ -0,0 +1,13 @@
+package net.oschina.git.roland.wimm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WellComeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_well_come);
+    }
+}
