c5ea9e7a2968c71c8246f5daf8dc198ec9821163
Roland <MarksVision@163.com>
2017-04-25 11:07:35 +0800

    fix a layout bug of common header

--- app/src/main/java/net/oschina/git/roland/wimm/function/rental/RentalAssistantActivity.java
+++ app/src/main/java/net/oschina/git/roland/wimm/function/rental/RentalAssistantActivity.java
@@ -1,21 +1,21 @@
 package net.oschina.git.roland.wimm.function.rental;
 
-import android.os.Bundle;
-
 import net.oschina.git.roland.wimm.R;
 import net.oschina.git.roland.wimm.common.base.BaseActivity;
+import net.oschina.git.roland.wimm.common.view.CommonHeader;
 
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_rental_assistant)
 public class RentalAssistantActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rental_assistant);
-    }
+    @ViewInject(R.id.header)
+    private CommonHeader header;
 
     @Override
     protected void initComp() {
-
+        header.setTitle(R.string.str_rental_assistant);
     }
 
     @Override
